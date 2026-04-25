59605b1de96df3872c16f3762ae89a4710bdccc5
Habtamuzeleke <habtamuzeleke74@gmail.com>
2026-04-25 21:46:41 +0300

    fixed ci error

--- src/main/java/com/ctbe/habtamu/product_service/service/ProductService.java
+++ src/main/java/com/ctbe/habtamu/product_service/service/ProductService.java
@@ -4,9 +4,11 @@ import com.ctbe.habtamu.product_service.dto.ProductResponse;
 import com.ctbe.habtamu.product_service.exception.ResourceNotFoundException;
 import com.ctbe.habtamu.product_service.model.Product;
 import com.ctbe.habtamu.product_service.repository.ProductRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+@Service
 public class ProductService {
     private final ProductRepository productRepository;
     // Constructor injection — preferred over @Autowired on a field
